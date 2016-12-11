b8455bc821116da21c619a6872fe03ac2cd04ae6
Jason van den Berg <jayvdb1@gmail.com>
2016-12-11 16:26:46 +0200

    Added Http server boilerplate

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,31 @@
-console.log('Server starting...');
+import express from "express";
+import http from "http";
+
+import {isDevelopment} from "./settings";
+
+// ----------------------
+//Setup
+const app = express();
+const server = new http.Server(app);
+
+// ----------------------
+//Configuration
+app.set("view engine", "pug"); //Using Pug templating
+app.use(express.static("public")); //Serve everything inside /public dir
+
+const useExternalStyles = !isDevelopment;
+const scriptRoot = isDevelopment ? "http://localhost:8080/build" : "/build";
+
+app.get("*", (req, res) => {
+	res.render("index", {
+		useExternalStyles,
+		scriptRoot
+	});
+});
+
+// ----------------------
+//Startup
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+	console.log(`Started http server on ${port}`);
+});
